5ad6d763f327a803431a3e40ce78c9f81c8bbbc3
Connor Todd <connortodd0304@gmail.com>
2020-08-20 13:31:15 +0100

    initial javascript solution implemented

---
+++ script.js
@@ -0,0 +1,50 @@
+// We get the player scoreboard and set it to 0 to start the game
+let playerScore = document.getElementById('playerScore');
+playerScore.textContent = 0;
+
+// We get the computer scoreboard and set it to 0 to start the game
+let computerScore = document.getElementById('computerScore');
+computerScore.textContent = 0;
+
+function handleRoundPoints(computerChoice, losingOption) {
+    if (computerChoice === losingOption) {
+        alert('Player Wins')
+        playerScore.textContent++;
+    } else {
+        alert('Computer Wins')
+        computerScore.textContent++;
+    }
+}
+
+function handlePlayerChoice(event) {
+    // This is used to create either 0,1,2 and this number is used to access the 
+    // gameButtons array 
+    let randomNumberGenerator = Math.floor(Math.random() * 3)
+
+    let playerChoice = event.currentTarget.id;
+    let computerChoice = gameButtons[randomNumberGenerator].id
+
+    // If the playerChoice is the same as the computerChoice it is a draw
+    if (playerChoice === computerChoice) {
+        alert('It is a draw')
+    } else {
+
+        // We then check the playerChoice, then calling handleRoundPoints
+        // passing the computerChoice with the option it can lose to
+        if (playerChoice === 'rock') handleRoundPoints(computerChoice, 'scissors')
+        if (playerChoice === 'paper') handleRoundPoints(computerChoice, 'rock')
+        if (playerChoice === 'scissors') handleRoundPoints(computerChoice, 'paper')
+
+    }
+
+}
+
+// get all of the game buttons into an array
+const gameButtons = document.getElementsByClassName('gameButton');
+
+// loop through all of the buttons and add an event listener to each button
+// this is used to handle the players choice
+for (let i = 0; i < gameButtons.length; i++) {
+    gameButtons[i].addEventListener('click', handlePlayerChoice)
+}
+
