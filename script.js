4eae45ff02b40cac894568ab21cf35f9d455d2f3
Connor Todd <connortodd0304@gmail.com>
2020-08-21 17:47:38 +0100

    update solution to match stage-6

--- script.js
+++ script.js
@@ -4,6 +4,21 @@ let playerScore = document.getElementById('playerScore');
 // We get the computer scoreboard and set it to 0 to start the game
 let computerScore = document.getElementById('computerScore');
 
+function computerChoiceGenerator() {
+    // An array containing all of the possible game options
+    const gameOptions = ['rock', 'paper', 'scissors']
+
+    // Generates a random number either being 0,1,2
+    let randomNumberGenerator = Math.floor(Math.random() * 3)
+
+    // The randomNumberGenerator variable is used to access a random value
+    // from the game options array
+    const randomComputerChoice = gameOptions[randomNumberGenerator]
+
+    return randomComputerChoice;
+}
+
+
 function handleRoundPoints(computerChoice, losingOption) {
     if (computerChoice === losingOption) {
         alert('Player Wins')
@@ -14,13 +29,18 @@ function handleRoundPoints(computerChoice, losingOption) {
     }
 }
 
-function handlePlayerChoice(event) {
-    // This is used to create either 0,1,2 and this number is used to access the 
-    // gameButtons array 
-    let randomNumberGenerator = Math.floor(Math.random() * 3)
 
+// this function is used to handle the players decision
+function handlePlayerChoice(event) {
+    // the event comes from the game button click
+    // this then takes the id value from each button
     let playerChoice = event.currentTarget.id;
-    let computerChoice = gameButtons[randomNumberGenerator].id
+
+    // the computer choice has a called computerChoiceGenerator 
+    // which returns randomly either rock, paper or scissors
+    let computerChoice = computerChoiceGenerator()
+
+
 
     // If the playerChoice is the same as the computerChoice it is a draw
     if (playerChoice === computerChoice) {
@@ -32,10 +52,10 @@ function handlePlayerChoice(event) {
         if (playerChoice === 'rock') handleRoundPoints(computerChoice, 'scissors')
         if (playerChoice === 'paper') handleRoundPoints(computerChoice, 'rock')
         if (playerChoice === 'scissors') handleRoundPoints(computerChoice, 'paper')
-
     }
 
 }
+
 
 // get all of the game buttons into an array
 const gameButtons = document.getElementsByClassName('gameButton');
@@ -45,4 +65,3 @@ const gameButtons = document.getElementsByClassName('gameButton');
 for (let i = 0; i < gameButtons.length; i++) {
     gameButtons[i].addEventListener('click', handlePlayerChoice)
 }
-
