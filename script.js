41249527649664f65afe7d6c0fe87401b577c084
Wilfredo <casas.farach@gmail.com>
2020-09-24 16:42:36 +0200

    shifumi completed

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-// Write your javaScript code here
+const shifumiArray = ["rock", "paper", "scissors"];
+
+function generateComputerChoice() {
+    console.log("function generateComputerChoice")
+  let randomItem =
+    shifumiArray[Math.floor(Math.random() * shifumiArray.length)];
+  return randomItem;
+}
+
+function drawOrNot(playerChoice) {
+    console.log("function drawOrNot")
+    computerChoice = generateComputerChoice(); //I called the gunction generatecomputerchoice
+  if (playerChoice === computerChoice) {
+    alert("its a draw");
+  } else {
+    winOrLose(playerChoice, computerChoice); // passing arguments
+  }
+}
+
+function winOrLose(playerChoiceX, computerChoiceY) {
+    console.log("function winOrLose")
+//    console.log("computer choice?", computerChoice) // I CAN NOT ACCESS COMPUTER CHOICE BECAUSE IT IS DEFINED INSIDE ANOTHER FUNCTION
+  // receiving parameters
+  if (
+    (playerChoiceX === "scissors" && computerChoiceY === "rock") ||
+    (playerChoiceX === "rock" && computerChoiceY === "paper") ||
+    (playerChoiceX === "paper" && computerChoiceY === "scissors")
+  ) {
+    alert("You lose!");
+  } else {
+    alert("You win!");
+  }
+}
